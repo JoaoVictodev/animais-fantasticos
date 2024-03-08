a05a07f4316670d626eee1d5fdd35388e9efb2cf
João Victor <themaciel190@gmail.com>
2024-03-08 10:02:01 -0500

    funcionamento refatorado

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,20 +1,41 @@
-export default function initFuncionamento() {
-  const funcionamento = document.querySelector('[data-semana]');
-  const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
-  const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
-  
-  
-  const dataAgora = new Date();
-  const diaAgora = dataAgora.getDay();
-  const horarioAgora = dataAgora.getHours();
-  
-  const semanaAberta = diasSemana.indexOf(diaAgora) !== -1;
-  const horarioaberto = horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1]
-  
-  if (semanaAberta && horarioaberto) {
-    funcionamento.classList.add('aberto')
-  } else {
-    funcionamento.classList.add('fechado')
+export default class Funcionamento {
+  constructor(funcionamento, activeClass){
+   this.funcionamento = document.querySelector(funcionamento);
+   this.activeClass = activeClass
+
   }
+
+  dadosFuncionamento(){
+    this.diasSemana = this.funcionamento.dataset.semana.split(',').map(Number);
+    this.horarioSemana = this.funcionamento.dataset.horario.split(',').map(Number);
+  }
+
+  
+  dadosAgora(){
+    this.dataAgora = new Date();
+    this.diaAgora = this.dataAgora.getDay();
+    this.horarioAgora = this.dataAgora.getUTCHours() - 3;
+  }
+
+  estaAberto(){
+    this.semanaAberta = this.diasSemana.indexOf(this.diaAgora) !== -1;
+    this.horarioaberto = this.horarioAgora >= this.horarioSemana[0] && this.horarioAgora < this.horarioSemana[1];
+    return this.semanaAberta && this.horarioaberto
+  }
+
+  ativaAberto(){
+    if(this.estaAberto()){
+       this.funcionamento.classList.add(this.activeClass);
+    }
+  }
+
+  init(){
+    if(this.funcionamento){
+      this.dadosFuncionamento();
+      this.dadosAgora();
+      this.ativaAberto();
+    }
+  }
+
 }
 
